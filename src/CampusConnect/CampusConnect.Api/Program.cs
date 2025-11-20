3639dbd2e919d0f1a49813de4dc58c89ffd571c2
Irina <irina-maria.istrate@s.unibuc.ro>
2025-11-20 15:31:00 +0200

    commit for merge

--- src/CampusConnect/CampusConnect.Api/Program.cs
+++ src/CampusConnect/CampusConnect.Api/Program.cs
@@ -6,31 +6,38 @@ using Microsoft.EntityFrameworkCore;
 using CampusConnect.Domain.Services;
 using CampusConnect.Api.Services;
 
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000", // Adresa React standard
+                                              "https://localhost:3000",
+                                              "http://localhost:5173", // Adresa Vite (dacă folosești)
+                                              "https://localhost:7000") // Sau orice port folosește frontend-ul tău
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddHttpContextAccessor(); // Asigură accesul la HttpContext
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 const string CorsPolicy = "Frontends";
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(CorsPolicy, policy =>
-        policy.WithOrigins(
-                "http://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            );
-});
 
 builder.Services.AddControllers();
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseCors(CorsPolicy);  
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseHttpsRedirection(); 
+app.UseCors(MyAllowSpecificOrigins);
 
+app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
