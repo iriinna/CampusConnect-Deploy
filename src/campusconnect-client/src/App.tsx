e44df52899c91be0ddb3d2da6502894460bf42b7
Irina <irina-maria.istrate@s.unibuc.ro>
2025-12-04 01:41:58 +0200

    Home page

--- src/campusconnect-client/src/App.tsx
+++ src/campusconnect-client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ConfirmEmail from './pages/ConfirmEmail';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
@@ -12,39 +13,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/confirm-email" element={<ConfirmEmail />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/profile" element={<ViewProfile />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-function Dashboard() {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
-  
-  return (
-    <div style={{ padding: '40px', textAlign: 'center' }}>
-      <h1>Dashboard - CampusConnect</h1>
-      <p>Bun venit, {user.firstName} {user.lastName}!</p>
-      <p>Email: {user.email}</p>
-      <button 
-        onClick={() => {
-          localStorage.clear();
-          window.location.href = '/login';
-        }}
-        style={{
-          padding: '10px 20px',
-          background: '#dc3545',
-          color: 'white',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer',
-          marginTop: '20px'
-        }}
-      >
-        Logout
-      </button>
-    </div>
-  );
-}
-
 export default App;
---
+++ src/campusconnect-client/src/pages/Home.tsx
@@ -0,0 +1,38 @@
+
+function Home() {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Dashboard - CampusConnect</h1>
+      <p>Bun venit, {user.firstName} {user.lastName}!</p>
+      <p>Email: {user.email}</p>
+      <button
+        onClick={() => {
+          window.location.href = '/ViewProfile';
+        }}
+      >
+        View Profile
+      </button>
+      <button 
+        onClick={() => {
+          localStorage.clear();
+          window.location.href = '/login';
+        }}
+        style={{
+          padding: '10px 20px',
+          background: '#dc3545',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          marginTop: '20px'
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Home;
