9e601ae29ce1f117eb5294ca512b49311142c818
Irina <irina-maria.istrate@s.unibuc.ro>
2025-12-22 01:25:59 +0200

    c

--- src/campusconnect-client/src/App.tsx
+++ src/campusconnect-client/src/App.tsx
@@ -7,6 +7,10 @@ import ViewProfile from './pages/User/ViewProfile';
 import EditProfile from './pages/User/EditProfile';
 import Announcements from './pages/Announcements';
 import CreateAnnouncement from './pages/CreateAnnouncement';
+import CreateEvent from './pages/Event/CreateEvent';
+import EditEvent from './pages/Event/UpdateEvent';
+import ViewEvent from './pages/Event/ViewEvent';
+import UpcomingEvents from './pages/Event/UpcomingEvent';
 import './App.css';
 
 function App() {
@@ -22,6 +26,11 @@ function App() {
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/announcements" element={<Announcements />} />
         <Route path="/create-announcement" element={<CreateAnnouncement />} />
+        <Route path="/events" element={<UpcomingEvents />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/edit-event/:id" element={<EditEvent />} />
+        <Route path="/event/:id" element={<ViewEvent />} />
+
       </Routes>
     </BrowserRouter>
   );
